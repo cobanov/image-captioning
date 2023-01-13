2c03e50a7bb2904e53624f64117728f860b28e85
Mert Cobanov <mertcobanov@gmail.com>
2023-01-13 19:02:37 +0300

    image preperations moved to utils

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ import os
 from tqdm import tqdm
 import glob
 from PIL import Image
+from torchvision import transforms
+from torchvision.transforms.functional import InterpolationMode
 
 
 def read_images_from_directory(image_directory):
@@ -10,7 +12,7 @@ def read_images_from_directory(image_directory):
     list_of_images = []
     for ext in ("*.gif", "*.png", "*.jpg"):
         list_of_images.extend(glob.glob(os.path.join(image_directory, ext)))
-    print(f'Images found: {len(list_of_images)}')
+    print(f"Images found: {len(list_of_images)}")
 
     return list_of_images
 
@@ -26,3 +28,23 @@ def read_with_pil(list_of_images, resize=False):
         pil_images.append(img)
 
     return pil_images
+
+
+def prep_images(pil_images):
+    image_size = 384
+    transform = transforms.Compose(
+        [
+            transforms.Resize(
+                (image_size, image_size), interpolation=InterpolationMode.BICUBIC
+            ),
+            transforms.ToTensor(),
+            transforms.Normalize(
+                (0.48145466, 0.4578275, 0.40821073),
+                (0.26862954, 0.26130258, 0.27577711),
+            ),
+        ]
+    )
+
+    # Transform Images
+    t_images = [transform(img).unsqueeze(0).to(device) for img in pil_images]
+    return t_images
