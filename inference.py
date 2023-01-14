a1f401f70adbadaf136a48e8dca108a036746be7
Mert Cobanov <mertcobanov@gmail.com>
2023-01-14 13:36:29 +0300

    argument parser fix

--- inference.py
+++ inference.py
@@ -9,6 +9,17 @@ import numpy as np
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+def init_parser(**parser_kwargs):
+    # CLI
+    parser = argparse.ArgumentParser(description="Image caption CLI")
+    parser.add_argument("-i", "--input", help="Input directoryt path, such as ./images")
+    parser.add_argument("-b", "--batch", help="Batch size", default=1, type=int)
+    parser.add_argument(
+        "-p", "--paths", help="A any.txt files contains all image paths."
+    )
+    return parser
+
+
 def init_model():
     # Model initialization
     #
@@ -28,36 +39,32 @@ def init_model():
 
 if __name__ == "__main__":
 
-    # CLI
-    parser = argparse.ArgumentParser(description="Image caption CLI")
-    parser.add_argument("-i", "--input", help="Input directoryt path, such as ./images")
-    parser.add_argument("-b", "--batch", help="Batch size", default=1, type=int)
-    parser.add_argument(
-        "-p", "--paths", help="A any.txt files contains all image paths."
-    )
+    parser = init_parser()
+    opt = parser.parse_args()
 
-    args = parser.parse_args()
-
-    if args.paths:  # If filepath.txt file does not exists
-        with open(args.paths, "r") as file:  #! Not tested yet
+    if opt.paths:  # If filepath.txt file does not exists
+        with open(opt.paths, "r") as file:  #! Not tested yet
             list_of_images = file.read()
     else:
-        list_of_images = utils.read_images_from_directory(args.input)
+        list_of_images = utils.read_images_from_directory(opt.input)
 
     # Batch processing
-    split_size = len(list_of_images) // args.batch
+    split_size = len(list_of_images) // opt.batch
     print(f"Split size: {split_size}")
     batches = np.array_split(list_of_images, split_size)
+
+    # Create directory if doesn't exists
+    utils.create_dir("captions")
 
     # Inference
     model = init_model()
     with torch.no_grad():
         print("Inference started")
         for batch_idx, batch in enumerate(batches):
-            pil_images = utils.read_with_pil(list_of_images)
+            pil_images = utils.read_with_pil(batch)
             transformed_images = utils.prep_images(pil_images, device)
 
-            with open("{batch_idx}_captions.txt", "w+") as file:
+            with open(f"captions/{batch_idx}_captions.txt", "w+") as file:
                 for path, image in zip(list_of_images, transformed_images):
 
                     caption = model.generate(
